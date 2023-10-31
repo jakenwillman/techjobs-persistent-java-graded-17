ba56ea44d85a35cac86a89d91d438ae2229e9c14
Jake Willman <jakenwillman@gmail.com>
2023-10-30 19:15:44 -0500

    completed task 1

--- queries.sql
+++ queries.sql
@@ -1,4 +1,12 @@
 --Part 1
+-- List the columns from the job table and their data types
+-- Column Name || Data Type
+------------------------------------
+-- id          || INT
+-- employer    || VARCHAR(255)
+-- name        || VARCHAR(255)
+-- skills      || VARCHAR(255)
+
 
 --Part 2
 
